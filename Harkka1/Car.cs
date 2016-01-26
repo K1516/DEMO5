6682481128552d52e09dbdd80de1c5852414e743
K1516 <k1516@student.jamk.fi>
2016-01-26 10:48:54 +0200

    VS testing

---
+++ Harkka1/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harkka1
+{
+    class Car
+    {
+        public string Vehicle { get; set; }
+        public string Model { get; set; }
+        public List<Tyres> Tyre { get; }
+        public Car()
+        {
+            Tyre = new List<Tyres>();
+        }
+        public void AddTyre(Tyres tyre)
+        {
+            Tyre.Add(tyre);
+        }
+        public override string ToString()
+        {
+            string sCar = "Car name: " + Vehicle + " Model: " + Model + " Tyres: \n";
+            foreach(Tyres tyre in Tyre)
+            {
+                sCar += tyre.ToString() + "\n";
+            }
+            return sCar;
+        }
+
+    }
+}
---
+++ Harkka1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harkka1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Tyres tyre = new Tyres();
+            tyre.Name = " Hankook";
+            tyre.Model = "Winter iPike";
+            tyre.Size = "235/45/R17";
+            Tyres tyr = new Tyres();
+            tyr.Name = "Hankook";
+            tyr.Model = "Winter iPike";
+            tyr.Size = "265/50/R17";
+
+            Car car = new Car();
+            car.Vehicle = "Mercedes";
+            car.Model = "E330";
+            car.AddTyre(tyre);
+            car.AddTyre(tyre);
+            car.AddTyre(tyr);
+            car.AddTyre(tyr);
+            Console.WriteLine(car.ToString());
+
+
+            Console.ReadLine();
+        }
+    }
+}
