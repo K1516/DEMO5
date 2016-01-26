dd134eef0f094e91ff39c2e347eab9ffc5716256
Risto Laine <k1516@student.jamk.fi>
2016-01-26 20:32:35 +0200

    Homework, cant get readyy

---
+++ Harj4/Blade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harj4
+{
+    class Blade
+    {
+        public string Shoot { get; set; }
+        public string Softness { get; set; }
+        public string Color { get; set; }
+        public override string ToString()
+        {
+            return "Shoot: "+ Shoot + " Softness " + Softness+ " Color: " + Color;
+        }
+    }
+}
---
+++ Harj4/Floorballstick.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harj4
+{
+    class Floorballstick
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Height { get; set; }
+        public List<Grip>Gri { get; }
+        public Floorballstick()
+        {
+            Gri = new List<Grip>();
+        }
+        public void AddGrip(Grip grip)
+        {
+            Gri.Add(grip);
+        }
+        public override string ToString()
+        {
+            string sStick = "Stick name: " + Name + " Model: " + Model + " Grip: \n";
+            foreach (Grip grip in Gri)
+            {
+                sStick += grip.ToString() + "\n";
+            }
+            return sStick;
+        }
+        public List<Blade> Bla { get; }
+
+        {
+            Bla = new List<Blade>();
+        }
+        public void AddGrip(Blade blade)
+        {
+            Bla.Add(blade);
+        }
+        public override string ToString()
+        {
+            string sStick = "Stick name: " + Name + " Model: " + Model + " Grip\n";
+            foreach (Blade blade in Bla)
+            {
+                sStick += blade.ToString() + "\n";
+            }
+            return sStick;
+        }
+    }
+}
